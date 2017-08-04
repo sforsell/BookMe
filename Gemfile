e15e23a6a44d228f18a28c81bc9e36f02f10f8a1
Sofia Forsell <email": null>
2017-08-04 14:18:05 -0400

    some modifications

--- Gemfile
+++ Gemfile
@@ -17,6 +17,11 @@ gem 'bcrypt'
 gem 'pry'
 gem 'pry-byebug'
 
+gem 'google-api-client', '0.8.2', :require => 'google/api_client'
+gem 'signet', '>=0.4.5'
+gem 'json'
+
+
 group :test do
   gem 'shoulda-matchers'
   gem 'rack-test'
--- app/controllers/google.rb
+++ app/controllers/google.rb
@@ -1,7 +1,19 @@
 get "/" do
-  "hello world!"
+  erb :"index"
 end
 
 get "/google816363bc72ff84b0.html" do
   send_file File.expand_path('google816363bc72ff84b0.html')
+end
+
+get "/success" do
+
+end
+
+get "/quickstart" do
+  erb :"quickstart"
+end
+
+get "/username" do
+  BASE_URL + SCHED_QUERY
 end
